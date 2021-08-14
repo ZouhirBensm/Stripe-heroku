70c4e9c99e4ce890489024268aa8db4eadf752d9
Zouhir Mohamed <Zouhir@Zouhirs-Mac-mini.local>
2021-08-14 12:19:32 -0400

    fixed keys file

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-if(process.env.NOD_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
   module.exports = require('./keys._prod')
 } else {
   module.exports = require('./keys._dev')
